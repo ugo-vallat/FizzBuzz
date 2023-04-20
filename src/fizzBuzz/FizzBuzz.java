39402737b46fd7042ea5c0cd9525e1db7958e796
ugoCodePasBeaucoup <max.planck.bg@gmail.com>
2023-04-20 16:49:17 +0200

    fizzBuzz repond fizz pour 3

--- src/fizzBuzz/FizzBuzz.java
+++ src/fizzBuzz/FizzBuzz.java
@@ -2,7 +2,11 @@ package fizzBuzz;
 
 public class FizzBuzz {
 
+	private static final String FIZZ = "fizz";
+
 	public String fizzBuzz(int num) {
+		if (num == 3)
+			return FIZZ;
 		return Integer.toString(num);
 	}
 
--- test/fizzBuzz/FizzBuzzTest.java
+++ test/fizzBuzz/FizzBuzzTest.java
@@ -16,13 +16,18 @@ class FizzBuzzTest {
 	}
 
 	@Test
-	void repond1() {
+	void fizzbuzz_repond_1_pour_1() {
 		assertEquals("1", fb.fizzBuzz(1));
 	}
 
 	@Test
-	void repond2() {
+	void fizzbuzz_repond_2_pour_2() {
 		assertEquals("2", fb.fizzBuzz(2));
+	}
+
+	@Test
+	void fizzbuzz_repond_fizz_pour_3() {
+		assertEquals("fizz", fb.fizzBuzz(3));
 	}
 
 }
